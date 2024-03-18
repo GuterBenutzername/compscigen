a89607d9b6ea2d8967a59fdee8f3a0650315c5cc
Adam Y. Cole II <afewburritos@gmail.com>
2024-03-18 02:21:04 -0500

    feat: implement a pretty good mvp
    The vscode extension api and workflow might be the most fun I've had
    with programming in a long time. I guess Microsoft *can* do things
    right sometimes!

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "compscigen.generate",
+    async () => {
+      let three = await vscode.window.showInputBox({
+        prompt: "What was the hardest part?",
+      });
+      let four = await vscode.window.showInputBox({
+        prompt: "Was there anything interesting or tricky?",
+      });
+      await vscode.commands.executeCommand("editor.action.formatDocument");
+      let submission =
+        "<ol><li>None.</li><li><pre>" +
+        vscode.window.visibleTextEditors[0].document.getText() +
+        "</pre></li><li>" +
+        three +
+        "</li><li>" +
+        four +
+        "</li></ol>";
+      await vscode.env.clipboard.writeText(submission);
+      vscode.window.showInformationMessage("Submission copied to clipboard.");
+    }
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "compscigen" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('compscigen.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from compscigen!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
